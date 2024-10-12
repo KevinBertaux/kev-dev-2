50b56af4d93695deb1c7b9f51128ecb99c749177
Kevin BERTAUX <58264753+KevinBertaux@users.noreply.github.com>
2024-10-12 18:29:16 +0200

    Cleaning installation

---
+++ src/ts/main.ts
@@ -0,0 +1,10 @@
+import '../scss/reset.scss'
+import '../scss/helpers.scss'
+import '../scss/style.scss'
+
+const welcomeMessage = "✅ It's working! Ready to code 💻";
+console.log(`${welcomeMessage}`);
+
+export function add(a: number, b: number) {
+    return a + b;
+}
